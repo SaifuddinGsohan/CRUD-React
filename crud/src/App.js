a4cfa518ebd346ae9e2f7a501b2a23d323dcde5b
saifuddinGsohan <sm.sohanbd504@gmail.com>
2022-04-01 19:35:59 +0600

    CRUD start  - S1 - SaiaN

---
+++ crud/src/App.js
@@ -0,0 +1,13 @@
+// SaiaN
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
